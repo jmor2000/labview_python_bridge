e3a57a8932f11009de543ac65309353fd0bfbffe
jeff morgan <74370093+jmor2000@users.noreply.github.com>
2026-04-02 15:24:19 +0100

    echoes 1

---
+++ code_basic/python_app/myapp_m.py
@@ -0,0 +1,62 @@
+import queue, json
+from modules.pyQserver import start_server, connect_to_server, terminate_server
+
+def main():
+    # Configuration - In a real project, these could come from a .env file
+    CONFIG = {
+        "auth": 'abracadabra',
+        "ip": "localhost",
+        "port": 50000,
+        "max_size": 1000
+    }
+    # 2. Connect Client ------------------------------------------
+    q_in, q_out = connect_to_server(CONFIG["ip"], CONFIG["port"], CONFIG["auth"])
+    print("📥 System ready. Waiting for LabVIEW data...")
+
+    # 3. Processing Loop -----------------------------------------
+    while True:
+        try:
+        # Operation...........
+            flag_process_data = False
+
+            # A.Read DATA
+            raw_data = q_in.get(timeout=0.1)  
+            # B.Validate - input data is JSON
+            try:
+                msg = json.loads(raw_data)
+            except json.JSONDecodeError:
+                print(f"⚠️ Received malformed data: {raw_data}")
+                flag_process_data = False
+                
+            # B.Validate - input item is present
+            if 'double' in msg:
+                flag_process_data = True
+
+            # C. Process Data
+            if flag_process_data == True:
+
+                val = float(msg['double'])
+                print(f"📈 Processing: {val}")
+                # note: I am updating the send data JSON with a new value
+                msg['double'] = val + 10.0
+                
+                # D. Send data back =====
+                q_out.put(json.dumps(msg))
+            
+        # Catch Error............
+        except queue.Empty:
+            # No data received within timeout, just loop again
+            continue
+        except KeyboardInterrupt:
+            # Exit the loop
+            print(f"📥 loop ending...")
+            break
+        except Exception as e:
+            print(f"❌ Error in loop: {e}")
+            break
+
+    #<<<-----Processing Loop END
+    print("end.")
+
+if __name__ == "__main__":
+    main()
---
+++ code_basic/python_app/myapp_run_m.cmd
@@ -0,0 +1,3 @@
+title Your Python App
+py myapp_m.py
+cmd /k
